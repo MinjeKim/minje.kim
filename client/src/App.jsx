d1e77b29bf7406ae3b87eb1aaaee30728fd6592e
minje <minje1409@gmail.com>
2022-10-27 20:13:08 +0900

    compo add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import {Footer} from './component/Footer.jsx';
+import {Navbar} from './component/Navbar.jsx';
+import {Home} from './component/Home.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
+        <Home />
       </header>
+      <footer className="App-footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+export const Navbar = () => {
+  return (
+    <>
+      <div>
+        LOGO
+      </div>
+      <div className='div-menu'>
+        <ul>
+            <li>PROFILE</li>
+            <li>BOARD</li>
+            <li>PORTFOLIOS</li>
+            <li>CONTACT</li>
+            <li>MY PAGE</li>
+        </ul>
+      </div>
+      <div className='div-state'>
+        Login
+      </div>
+    </>
+  );
+}
